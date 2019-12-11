b504c941e75c2f8bb0b1d2e58fb1c6fe78ff90be
priyasoran <58772948+priyasoran@users.noreply.github.com>
2019-12-12 00:35:10 +0530

    Task5

--- Calculator.php
+++ Calculator.php
@@ -1,27 +1,33 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-	$argvStr = isset($argv[2]) ? $argv[2] : null; //get value to perform Operation
+	$argvStr = isset($argv[2]) ? $argv[2] : null;  //get value to perform Operation
 	preg_match_all('/-?\\d+(?:\\.\\d+)?/m', $argvStr, $argvArr); // get only numeric values from string
+	//print_r($argvArr); die;
 	$argvArr = $argvArr[0];
-		switch ($argv[1]) {
-			//case to add 2 Numbers
-			case "sum":
-				if (isset($argvArr) && (sizeof($argvArr) <= 2)) {
-					Calculator\CalculatorAssignment::sumTwo($argvArr);
-				} else {
-					echo "Please provide appropriate input. More than two values are not allowed.";
-				}
-				break;
-				//Case to add multiple Numbers
-			case "add":
-				if (isset($argvArr)) {
-					Calculator\CalculatorAssignment::sumTwo($argvArr);
-				} else {
-					echo "Please provide appropriate input.";
-				}
-				break;
-			default:
-				echo "You are not using the appropriate operation!";
+		$isNegativeNumber = Calculator\CalculatorAssignment::checkNegativeNumber($argvArr); // Check If Any Negative Number Exist in Given String
+		if($isNegativeNumber){
+			switch ($argv[1]) {
+				//case to add 2 Numbers
+				case "sum":
+					if (isset($argvArr) && (sizeof($argvArr) <= 2)) {
+						Calculator\CalculatorAssignment::sumTwo($argvArr);
+					} else {
+						echo "Please provide appropriate input. More than two values are not allowed.";
+					}
+					break;
+					//Case to add multiple Numbers
+				case "add":
+					if (isset($argvArr)) {
+						Calculator\CalculatorAssignment::sumTwo($argvArr);
+					} else {
+						echo "Please provide appropriate input.";
+					}
+					break;
+				default:
+					echo "You are not using the appropriate operation!";
+			}
+		}else{
+			echo "Negative numbers are not allowed.";
 		}
 ?>
--- app/CalculatorAssignment.php
+++ app/CalculatorAssignment.php
@@ -1,12 +1,27 @@
 <?php
 namespace Calculator;
-	/*
+
+  class CalculatorAssignment {
+	  /*
 	  Use : To add all the values in Array
 	  */
-  class CalculatorAssignment {
     static function sumTwo($params){
       $sum = array_sum($params);
 	  echo  $sum;
     }
+	/*
+	  Use : Check If Array contaning negative Number
+	  */
+	static function checkNegativeNumber($params){
+		foreach ($params as $value)
+		{
+			if ($value < 0)
+			{
+				 return 0;
+			}
+		}
+		return 1;
+    }
+	
   }
   ?>
