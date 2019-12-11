7866443a5ec1f22c65af42827010404019452345
priyasoran <58772948+priyasoran@users.noreply.github.com>
2019-12-12 00:34:23 +0530

    Task4

--- app/CalculatorAssignment.php
+++ app/CalculatorAssignment.php
@@ -1,10 +1,9 @@
 <?php
 namespace Calculator;
-
-  class CalculatorAssignment {
-	  /*
+	/*
 	  Use : To add all the values in Array
 	  */
+  class CalculatorAssignment {
     static function sumTwo($params){
       $sum = array_sum($params);
 	  echo  $sum;
